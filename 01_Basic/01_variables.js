45bc6b6d9e48deed1ce54c8efca7b6e6c7a60f95
hariprakash25 <hariprakashverma9648@gmail.com>
2025-10-07 20:05:38 +0000

    container config done

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "hari@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState = "8756";
+
+//accountId = 2// not allowed
+accountEmail = "hari@hp.com"
+accountPassword = "382627"
+accountCity = "new york"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
