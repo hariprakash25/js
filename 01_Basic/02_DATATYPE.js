caf35264605b2ebe20c8704e88bb60351d1fdf4b
hariprakash25 <hariprakashverma9648@gmail.com>
2025-10-08 19:56:02 +0000

    DATATYPES

---
+++ 01_Basic/02_DATATYPE.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Hari")
+
+
+let name = "Hari"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//primitive  datatypes
+// number ->  2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> 
+//symbol -> unique
+
+
+// object
+
+console.log(typeof "Hari")
+
+// tc39.es,mdn ->  learning website
